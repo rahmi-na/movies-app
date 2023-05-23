20388e6a7c4be5fc7cc34eea73f1d118d4eacec6
Rahmi Azizah <79822759+rahmi-na@users.noreply.github.com>
2023-05-23 21:14:48 +0800

    add slick

--- src/modules/Home/detail/upcoming/index.js
+++ src/modules/Home/detail/upcoming/index.js
@@ -1,7 +1,41 @@
 import React from "react";
+import Slider from "react-slick";
 
 function UpComing() {
-  return <div className="h-screen ">Upcoming</div>;
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: false,
+  };
+  return (
+    <div className="w-full flex items-center justify-center text-white ">
+      <div className="w-5/6 bg-slate-100">
+        <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
+      </div>
+    </div>
+  );
 }
 
 export default UpComing;
