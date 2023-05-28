715b0aa15b54f2ba8ff856f3159285c10e9e08d2
Rahmi Azizah <79822759+rahmi-na@users.noreply.github.com>
2023-05-29 01:13:34 +0800

    add footer

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,17 @@
-import NavigationBar from '@/components/common/NavigationBar';
-import { store } from '@/store';
-import '@/styles/globals.css'
-import { Provider } from 'react-redux';
+import FooterBar from "@/components/FooterBar";
+import NavigationBar from "@/components/common/NavigationBar";
+import { store } from "@/store";
+import "@/styles/globals.css";
+import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }) {
   return (
     <div>
       <Provider store={store}>
-      <NavigationBar />
-      <Component {...pageProps} />
+        <NavigationBar />
+        <Component {...pageProps} />
+        <FooterBar />
       </Provider>
     </div>
   );
-  
 }
---
+++ src/utils/footer.js
@@ -0,0 +1,75 @@
+import { FaFacebookF, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
+
+export const sosmed = [
+  {
+    Icons: FaFacebookF,
+  },
+  {
+    Icons: FaInstagram,
+  },
+  {
+    Icons: FaTwitter,
+  },
+  {
+    Icons: FaYoutube,
+  },
+];
+
+export const data = [
+  {
+    title: "Information",
+    desc: [
+      {
+        name: "Deskripsi Audio",
+      },
+      {
+        name: "Hubungan Investor",
+      },
+      {
+        name: "Informasi Legal",
+      },
+    ],
+  },
+  {
+    title: "Policy",
+    desc: [
+      {
+        name: "Pusat Bantuan",
+      },
+      {
+        name: "Lowongan Kerja",
+      },
+      {
+        name: "Preferensi Cookie",
+      },
+    ],
+  },
+  {
+    title: "About",
+    desc: [
+      {
+        name: "Kartu Hadiah",
+      },
+      {
+        name: "Ketentuan Penggunaan",
+      },
+      {
+        name: "Informasi Perusahaan",
+      },
+    ],
+  },
+  {
+    title: "Help",
+    desc: [
+      {
+        name: "Pusat Media",
+      },
+      {
+        name: "Privasi",
+      },
+      {
+        name: "Hubungi Kami",
+      },
+    ],
+  },
+];
