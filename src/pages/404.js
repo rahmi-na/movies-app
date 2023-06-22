88afeb922dd7c9ee7366f9246c0ed194fb813a97
Rahmi Azizah <79822759+rahmi-na@users.noreply.github.com>
2023-06-22 23:51:33 +0800

    upt

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ export default function NotFound() {
     <div>
       <div className="flex justify-center items-center text-white h-[calc(90vh-4rem)]">
         <div>
-          <h1 className=" text-lg font-thin underline underline-offset-[10px] md:underline-offset-[20px] decoration-[1px]">
+          <h1 className="text-lg font-thin underline underline-offset-[10px] md:underline-offset-[20px] decoration-[1px]">
             404 Page NotFound
           </h1>
         </div>
